7d91ad5f0d45cefa096000b77feec61687abfb59
Darek Rossman <darekrossman@me.com>
2025-04-07 12:21:10 -0400

    rollback, turn off prefetch

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -3,18 +3,9 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import Form from "next/form";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default function Search() {
   const searchParams = useSearchParams();
-  const query = searchParams?.get("q") || "";
-  const [searchQuery, setSearchQuery] = useState(query);
-
-  useEffect(() => {
-    setSearchQuery(query);
-  }, [query]);
-
-  console.log(searchQuery);
 
   return (
     <Form
@@ -23,21 +14,17 @@ export default function Search() {
       className="w-max-[550px] relative w-full lg:w-80 xl:w-full"
     >
       <input
+        key={searchParams?.get("q")}
         type="text"
         name="q"
         placeholder="Search for products..."
         autoComplete="off"
-        value={searchQuery}
-        onChange={(e) => {
-          console.log("setting", e.target.value);
-          setSearchQuery(e.target.value);
-        }}
+        defaultValue={searchParams?.get("q") || ""}
         className="text-md w-full rounded-lg border bg-white px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
       />
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <MagnifyingGlassIcon className="h-4" />
       </div>
-      <button type="submit">Submit</button>
     </Form>
   );
 }
