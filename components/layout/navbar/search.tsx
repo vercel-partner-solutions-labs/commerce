8330b3f4945340a6b76fce9f5fc8499bc0e5e96d
Darek Rossman <darekrossman@me.com>
2025-04-07 12:02:25 -0400

    debugging search

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -1,21 +1,30 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import Form from 'next/form';
-import { useSearchParams } from 'next/navigation';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Form from "next/form";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   const searchParams = useSearchParams();
+  const query = searchParams?.get("q") || "";
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    setSearchQuery(query);
+  }, [query]);
 
   return (
     <Form action="/search" className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
       <input
-        key={searchParams?.get('q')}
         type="text"
         name="q"
         placeholder="Search for products..."
         autoComplete="off"
-        defaultValue={searchParams?.get('q') || ''}
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
         className="text-md w-full rounded-lg border bg-white px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
       />
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
