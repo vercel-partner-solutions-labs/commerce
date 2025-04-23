7b36158508ba78bae88444d0baea38648c6e866b
Alex <alex.hawley@vercel.com>
2025-04-23 14:27:59 -0500

    Updated constants and imports
    - Moved defaultSort and sorting imports to lib/sfcc/constants.ts for better structure.
    - Updated the import path in the collection page to reflect the new location of constants.

--- lib/sfcc/constants.ts
+++ lib/sfcc/constants.ts
@@ -76,31 +76,6 @@ export const checkoutStepRoutes: Record<CheckoutStep, string> = {
   [CheckoutStep.Confirmation]: "/checkout/confirmation",
 };
 
-// Type for the state returned from our form actions. Provides
-// additional type safety for error fields.
-export type FormActionState<T extends z.ZodTypeAny = z.ZodTypeAny> =
-  | {
-      errors: {
-        formErrors?: string[];
-        fieldErrors?: z.inferFlattenedErrors<T>["fieldErrors"];
-      };
-    }
-  | undefined;
-
-export enum CheckoutStep {
-  Information = 1,
-  Shipping,
-  Payment,
-  Confirmation,
-}
-
-export const checkoutStepRoutes: Record<CheckoutStep, string> = {
-  [CheckoutStep.Information]: "/checkout/information",
-  [CheckoutStep.Shipping]: "/checkout/shipping",
-  [CheckoutStep.Payment]: "/checkout/payment",
-  [CheckoutStep.Confirmation]: "/checkout/confirmation",
-};
-
 export type CountryCode = "US" | "CA" | "UK";
 
 export type PostalCodeConfig = {
